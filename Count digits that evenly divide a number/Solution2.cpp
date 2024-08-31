5209f366517abf46d6995a4c6125103943c64f91
Ankit Pathak <workwithankit2002@gmail.com>
2024-08-31 22:10:51 +0530

    Update Solution2.cpp

--- Count digits that evenly divide a number/Solution2.cpp
+++ Count digits that evenly divide a number/Solution2.cpp
@@ -1,14 +1,38 @@
-int countDigits(int n){
-	// Write your code here.
-	int count = 0;
-	std::string str = to_string(n);
-	for(char s : str) {
-		int num = int(s - '0');
-		if(s != '0') {
-			if (n % num == 0) {
-			count++;
-			}
-		}
-	}
-	return count;	
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        std::string str = to_string(N);
+        int count = 0;
+        for(char s : str) {
+            int num = int(s - '0');
+            if(num != 0) {
+                if(N % num == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
 }
+// } Driver Code Ends
