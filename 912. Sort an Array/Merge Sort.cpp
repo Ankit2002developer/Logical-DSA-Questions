23eb8b320b73a25e4d9c435167a5017ffa8144e2
Ankit Pathak <workwithankit2002@gmail.com>
2024-09-03 00:19:51 +0530

    Update Merge Sort.cpp

--- 912. Sort an Array/Merge Sort.cpp
+++ 912. Sort an Array/Merge Sort.cpp
@@ -1,59 +1,84 @@
-#include <iostream>
-#include <array>
+
+//{ Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
-void merge(int arr[], int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
 
-    int L[n1], R[n2];
-
-    for (int i = 0; i < n1; i++) {
-        L[i] = arr[l + i];
-    }
-
-    for (int j = 0; j < n2; j++) {
-        R[j] = arr[m + 1 + j];
-    }
-
-    int i = 0;
-    int j = 0;
-    int k = l;
-
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
 }
 
-void mergeSort(int arr[], int l, int r) {
-    if (l >= r) {
-        return;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void merge(int arr[], int l, int m, int r)
+    {
+         // Your code here
+         vector<int> temp;
+         int left = l;
+         int right = m + 1;
+         while(left <= m && right <= r) {
+             if(arr[left] <= arr[right]) {
+                 temp.push_back(arr[left]);
+                 left++;
+             } else {
+                 temp.push_back(arr[right]);
+                 right++;
+             }
+         }
+         while(left <= m) {
+             temp.push_back(arr[left]);
+             left++;
+         }
+         while(right <= r) {
+             temp.push_back(arr[right]);
+             right++;
+         }
+         for(int i = l; i <= r; i++) {
+             arr[i] = temp[i - l];
+         }
     }
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+        int m = l + ((r - l) / 2);
+        if(l >= r) return;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+        merge(arr, l, m, r);
+    }
+};
 
-    int m = l + (r - l) / 2;
+//{ Driver Code Starts.
 
-    mergeSort(arr, l, m);
-    mergeSort(arr, m + 1, r);
 
-    merge(arr, l, m, r);
+int main()
+{
+    int n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+    
+    scanf("%d",&n);
+    int arr[n+1];
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.mergeSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
 }
+// } Driver Code Ends
