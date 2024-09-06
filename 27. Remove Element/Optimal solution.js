fc7d84a92a2ad73e90a6e110021ab0d231b70e67
Ankit Pathak <workwithankit2002@gmail.com>
2024-09-06 23:18:22 +0530

    Create Optimal solution.js

---
+++ 27. Remove Element/Optimal solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let n = nums.length;
+    let i = 0;
+    for(let j = 0; j < n; j++) {
+        if(nums[j] !== val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+};
