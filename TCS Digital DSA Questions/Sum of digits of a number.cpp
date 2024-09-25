ffefbebf302886403deda9895e67d934034563be
Ankit Pathak <workwithankit2002@gmail.com>
2024-09-25 15:29:15 +0530

    Update Sum of digits of a number.cpp

--- TCS Digital DSA Questions/Sum of digits of a number.cpp
+++ TCS Digital DSA Questions/Sum of digits of a number.cpp
@@ -25,3 +25,35 @@ int main() {
     }
     return 0;
 }
+
+
+// alternate solution
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+// Calculate the sum of digits of a number
+
+long long sumOfDigits(long long num) {
+    long long sum = 0;
+    string str = to_string(num);
+    for(char ch : str) {
+        int digit = int(ch - '0');
+        sum += digit;
+    }
+    return sum;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        long long num;
+        cin >> num;
+        cout << "Sum of digits of " << num << " is ";
+        cout << sumOfDigits(num) << endl;
+    }
+    return 0;
+}
